439dd598eff357634215cd12c67bfd9bfff124db
Lucio Tato <luciotato@gmail.com>
2023-07-29 13:38:27 -0300

    first

---
+++ README.md
@@ -0,0 +1,11 @@
+## Quick Start
+
+```
+# install packages
+pnpm install
+# build backend & ABI
+anchor build
+# run front end dev server
+cd packages/app
+pnpm dev
+```
--- packages/app/src/scenes/addEnergy/ui.tsx
+++ packages/app/src/scenes/addEnergy/ui.tsx
@@ -11,7 +11,8 @@ import { BN } from '@coral-xyz/anchor';
 import { minterAddress } from '../../pdas';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { TOKEN_PROGRAM_ID } from '@coral-xyz/anchor/dist/cjs/utils/token';
-import {Buffer} from 'buffer';
+import { Buffer } from 'buffer';
+import toast from 'react-simple-toasts';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).Buffer = Buffer;
@@ -66,68 +67,74 @@ const AddEnergyUi: FC = () => {
 
   const handleBuy = useCallback(() => {
     (async () => {
-      if (value > 0 && program && worldData && publicKey) {
-        const payment = value * worldData.energyPrice.toNumber();
-        const payFrom = getAssociatedTokenAddressSync(worldData.treasuryMint, publicKey);
-        const mintTo = getAssociatedTokenAddressSync(worldData.energyMint, publicKey);
-        const prepare: TransactionInstruction[] = [];
-        let have = 0;
-        const payFromInfo = await program.provider.connection.getParsedAccountInfo(payFrom);
-        if (payFromInfo.value) {
-          have = (payFromInfo.value as AccountInfo<ParsedAccountData>).data.parsed.info
-            .tokenAmount.amount;
-        } else {
-          prepare.push(
-            createAssociatedTokenAccountInstruction(
-              publicKey,
-              payFrom,
-              publicKey,
-              worldData.treasuryMint,
-            )
-          );
-        }
+      try {
+        if (value > 0 && program && worldData && publicKey) {
+          const payment = value * worldData.energyPrice.toNumber();
+          const payFrom = getAssociatedTokenAddressSync(worldData.treasuryMint, publicKey);
+          const mintTo = getAssociatedTokenAddressSync(worldData.energyMint, publicKey);
+          const prepare: TransactionInstruction[] = [];
+          let have = 0;
+          const payFromInfo = await program.provider.connection.getParsedAccountInfo(payFrom);
+          if (payFromInfo.value) {
+            have = (payFromInfo.value as AccountInfo<ParsedAccountData>).data.parsed.info
+              .tokenAmount.amount;
+          } else {
+            prepare.push(
+              createAssociatedTokenAccountInstruction(
+                publicKey,
+                payFrom,
+                publicKey,
+                worldData.treasuryMint,
+              )
+            );
+          }
 
-        const left = payment - have;
-        if (left > 0) {
-          prepare.push(
-            SystemProgram.transfer({
-              fromPubkey: publicKey,
-              toPubkey: payFrom,
-              lamports: left
-            })
-          );
-          prepare.push(
-            createSyncNativeInstruction(payFrom)
-          )
-        }
-
-        if (!(await program.provider.connection.getAccountInfo(mintTo))) {
-          prepare.push(
-            createAssociatedTokenAccountInstruction(
-              publicKey,
-              mintTo,
-              publicKey,
-              worldData.energyMint,
+          const left = payment - have;
+          if (left > 0) {
+            prepare.push(
+              SystemProgram.transfer({
+                fromPubkey: publicKey,
+                toPubkey: payFrom,
+                lamports: left
+              })
+            );
+            prepare.push(
+              createSyncNativeInstruction(payFrom)
             )
-          )
-        }
-        await program.methods
-          .mintEnergy(new BN(value))
-          .accountsStrict({
-            minter: minterAddress({
-              programId: program.programId,
+          }
+
+          if (!(await program.provider.connection.getAccountInfo(mintTo))) {
+            prepare.push(
+              createAssociatedTokenAccountInstruction(
+                publicKey,
+                mintTo,
+                publicKey,
+                worldData.energyMint,
+              )
+            )
+          }
+          await program.methods
+            .mintEnergy(new BN(value))
+            .accountsStrict({
+              minter: minterAddress({
+                programId: program.programId,
+                world: worldPk,
+              }),
               world: worldPk,
-            }),
-            world: worldPk,
-            energyMint: worldData.energyMint,
-            treasury: worldData.treasury,
-            payFrom,
-            payer: publicKey,
-            mintTo,
-            tokenProgram: TOKEN_PROGRAM_ID,
-          })
-          .preInstructions(prepare)
-          .rpc()
+              energyMint: worldData.energyMint,
+              treasury: worldData.treasury,
+              payFrom,
+              payer: publicKey,
+              mintTo,
+              tokenProgram: TOKEN_PROGRAM_ID,
+            })
+            .preInstructions(prepare)
+            .rpc()
+
+          toast("Success!",{className:"success"})
+        }
+      } catch (ex) {
+        toast(ex.message, {className:"error"})
       }
     })();
   }, [value, program, worldData, worldPk, publicKey]);
